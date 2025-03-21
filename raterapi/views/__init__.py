ac33f9c57b28d9d2f28841392e9ca1156f91abde
Ethan Brown <eb934053@gmail.com>
2025-03-21 11:22:22 -0500

    Created Game model. Added list and create options in Game View. Added API route for fetches

--- raterapi/views/__init__.py
+++ raterapi/views/__init__.py
@@ -1,2 +1,3 @@
 from .user import UserViewSet
 from .category_view import CategoryViewSet
+from .game_view import GameViewSet
--- raterproject/urls.py
+++ raterproject/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from raterapi.views import CategoryViewSet, UserViewSet
+from raterapi.views import CategoryViewSet, UserViewSet, GameViewSet
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r"categories", CategoryViewSet, "category")
+router.register(r"games", GameViewSet, "game")
 
 urlpatterns = [
     path("", include(router.urls)),
