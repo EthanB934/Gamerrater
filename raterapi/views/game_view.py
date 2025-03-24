8838e40299026b4d85ca6d98c9c8500e55a15a91
Ethan Brown <eb934053@gmail.com>
2025-03-24 13:41:49 -0500

    Added model and viewset for games resource

--- raterapi/views/game_view.py
+++ raterapi/views/game_view.py
@@ -3,14 +3,20 @@ from rest_framework.response import Response
 from raterapi.models import Game
 
 class GameSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Game
         fields = ("id", "user", "title", "picture_of_game", "description", "designer", "year_released", "player_count", "play_time", "age_to_play")
-
+        
 class GameViewSet(viewsets.ViewSet):
     def list(self, request):
         games = Game.objects.all()
         serializer = GameSerializer(games, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        game = Game.objects.get(pk=pk)
+        serializer = GameSerializer(game, many=False)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def create(self, request):
